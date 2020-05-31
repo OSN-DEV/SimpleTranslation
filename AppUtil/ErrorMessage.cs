89049fbe1e348fab92cd4e054b2766d4f4e59a65
OSN-DEV <osndev1973@gmail.com>
2020-05-31 18:19:44 +0900

    エラーワーディングを追加

--- AppUtil/ErrorMessage.cs
+++ AppUtil/ErrorMessage.cs
@@ -13,11 +13,13 @@ namespace SimpleTranslation.AppUtil {
             FailToLaunchSpread,
             FailToTranslate,
             FailToSave,
+            APIIsNotSet,
         }
         private static Dictionary<ErrMsgId, string> _message = new Dictionary<ErrMsgId, string>() {
             { ErrMsgId.FailToLaunchSpread, "スプレッドシートの表示に失敗しました。"},
             { ErrMsgId.FailToTranslate,"翻訳に失敗しました。" },
             { ErrMsgId.FailToSave,"保存に失敗しました。" },
+            { ErrMsgId.APIIsNotSet,"APIのURLが設定されていません。" },
         };
         #endregion
 
